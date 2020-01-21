b2e985c87002983d45bd33a700141207166c7bb4
BatchSource <53985751+BatchSource@users.noreply.github.com>
2020-01-21 11:49:22 -0600

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,9 +1,6 @@
 sudo apt-get install python3-pip
 sudo apt-get install python3-tk
-
 sudo pip3 install easygui
-sudo cp nordgui.desktop ~/Desktop
-sudo cp nordgui.desktop ~/.local/share/applications
 
 echo "Complete. You may now exit the terminal."
 sleep 99d
